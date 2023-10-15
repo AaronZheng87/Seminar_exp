54720a885714c4620bff06f404198fbd5dbf2154
AaronZheng87 <aaronzheng87@gmail.com>
2023-10-15 18:01:50 +0200

    figures

--- 3_Script/data_sim.R
+++ 3_Script/data_sim.R
@@ -1,97 +1,168 @@
-within <- list(
-  Valence = c(self = "self-related", 
-              other = "Other-related",
-              None = "None"),
-  Posit = c(High = "high", 
-            Low  = "low")
-)
 
+library(tidyverse)
+library(faux)
 
-
-design <- check_design(within, between, mu = mu, plot = FALSE)
-str(design$mu)
-mu <- data.frame(
-  row.names = c("con_easy", "con_med", "con_hard",
-                "inc_easy", "inc_med",  "inc_hard"),
-  novice_day = 10:15,
-  novice_night = 11:16,
-  expert_day = 9:14,
-  expert_night = 10:15
-)
-
-
-data.frame(
-  row.names = c("con_easy", "con_med", "con_hard",
-                "inc_easy", "inc_med",  "inc_hard"),
-  novice_day = 10:15,
-  novice_night = 11:16,
-  expert_day = 9:14,
-  expert_night = 10:15
-)
-
-r <- list(
-  novice_day = 0.3,
-  novice_night = 0.2,
-  expert_day = 0.5,
-  expert_night = 0.4
-)
-
-
-
-
-
-# upper right triangle correlation specification
-# inc and con have r = 0.5 within each difficultly level, 0.2 otherwise
-#          ce,  ie,  cm,  im,  ch,  ih
-triangle <-  c(0.5, 0.2, 0.2, 0.2, 0.2, #con_easy
-               0.2, 0.2, 0.2, 0.2, #inc_easy
-               0.5, 0.2, 0.2, #con_med
-               0.2, 0.2, #inc_med
-               0.5) #con_hard
-#inc_hard
-
-r <- list(
-  novice_day = triangle,
-  novice_night = triangle,
-  expert_day = triangle,
-  expert_night = triangle
-)
-df <- sim_design(within, between, n = 100, 
-                 mu = mu, sd = 2, r = r, 
-                 dv = c(rt = "Reaction Time"), 
-                 plot = FALSE, long = TRUE)
-
-head(df)
-
-hhh <- sim_design(
-  n = 50, # 生成50名被试
-  within = list(groupA = c("A1", "A2", "A3"), groupB = c("B1", "B2")), # 生成被试内因子的列表
+Distractor_sim <- sim_design(
+  n = 50, # 50 subjects
+  #contain two factor
+  within = list(Type = c("Self", "Other", "Neutral"), Position = c("High", "Low")), # 生成被试内因子的列表
   
-  mu = c(A1B1 = 0, A1B2 = 100, A2B1 = 200, A2B2=300, A3B1=400, A3B2=500), # 设置变量的均值
+  mu = c(self_high = 680, self_low = 900, other_high = 650, other_low=780, neutral_high=600, neutral_low=690), # 设置变量的均值
   
-  sd = c(A1B1 =20, A1B2 =40, A2B1 =60, A2B2=80, A3B1=100, A3B2=120), # 设置变量的标准差
+  sd = c(20, 30,25, 35, 28, 40), # 设置变量的标准差
   
   long = TRUE,  # 输出长数据形式
   
   dv = "score", # 因变量命名为score
   plot = FALSE   # 显示图
 )
+Distractor_sim %>% 
+  group_by(Type, Position) %>% 
+  summarise(m = mean(score), 
+            sd = sd(score)) %>% 
+  arrange()
 
-aaa <- sim_design(
+
+
+Non_distractor <- sim_design(
   n = 50, # 生成50名被试
-  within = list(groupA = c("A4")), # 生成被试内因子的列表
+  within = list(Type = c("None")), # 生成被试内因子的列表
   
-  mu = c(a=600), # 设置变量的均值
+  mu = c(a=500), # 设置变量的均值
   
-  sd = c(a=100), # 设置变量的标准差
+  sd = c(a=20), # 设置变量的标准差
   
   long = TRUE,  # 输出长数据形式
   
   dv = "score", # 因变量命名为score
   plot = FALSE   # 显示图
 ) %>% 
-  mutate(groupB = "None") %>% 
-  select(id, groupA, groupB, score)
+  mutate(Position = "None") %>% 
+  select(id, Type, Position, score)
 
 
-d = rbind(hhh, aaa)
+all <- rbind(Distractor_sim, Non_distractor)
+data_summary <- function(x) {
+  m <- mean(x)
+  ymin <- m-sd(x)
+  ymax <- m+sd(x)
+  return(c(y=m,ymin=ymin,ymax=ymax))
+}
+
+
+errbar_lims <- all %>% 
+  group_by(Type, Position) %>% 
+  summarize(mean  = mean(score), 
+            se    = sd(score) / sqrt(n()), 
+            upper = mean + se, 
+            lower = mean - se) %>% mutate(x_axis = case_when(
+              Type == "Self" & Position == "High" ~ 0.5, 
+              Type == "Other" & Position == "High" ~ 1, 
+              Type == "Neutral" & Position == "High" ~ 1.5, 
+              Type == "Self" & Position == "Low" ~ 3.5, 
+              Type == "Other" & Position == "Low" ~ 4, 
+              Type == "Neutral" & Position == "Low" ~ 4.5,
+              Type == "None" ~ 6.5
+            ))
+
+
+plot_dat <- all %>% mutate(x_axis = case_when(
+  Type == "Self" & Position == "High" ~ 0.5, 
+  Type == "Other" & Position == "High" ~ 1, 
+  Type == "Neutral" & Position == "High" ~ 1.5, 
+  Type == "Self" & Position == "Low" ~ 3.5, 
+  Type == "Other" & Position == "Low" ~ 4, 
+  Type == "Neutral" & Position == "Low" ~ 4.5,
+  Type == "None" ~ 6.5
+))
+
+ggplot(plot_dat) + 
+  geom_violin(data = plot_dat %>% filter(x_axis == 0.5), aes(x=x_axis, y=score, fill = "Self"), width=0.5) + 
+  geom_errorbar(data=errbar_lims%>% filter(x_axis == 0.5), 
+                aes(x = x_axis, ymax = upper, ymin = lower, group = Type), 
+                stat = 'identity', position = position_dodge(width = 0.1), width = 0.1) + 
+  geom_violin(data = plot_dat %>% filter(x_axis == 1), aes(x=x_axis, y=score, fill = "Other"), width=0.5) + 
+  geom_errorbar(data=errbar_lims%>% filter(x_axis == 1), 
+                aes(x = x_axis, ymax = upper, ymin = lower, group = Type), 
+                stat = 'identity', position = position_dodge(width = 0.1), width = 0.1) + 
+  geom_violin(data = plot_dat %>% filter(x_axis == 1.5), aes(x=x_axis, y=score, fill = "Neutral"), width=0.5) + 
+  geom_errorbar(data=errbar_lims%>% filter(x_axis == 1.5), 
+                aes(x = x_axis, ymax = upper, ymin = lower, group = Type), 
+                stat = 'identity', position = position_dodge(width = 0.1), width = 0.1) + 
+  geom_violin(data = plot_dat %>% filter(x_axis == 3.5), aes(x=x_axis, y=score, fill = "Self"),width=0.5) + 
+  geom_errorbar(data=errbar_lims%>% filter(x_axis == 3.5), 
+                aes(x = x_axis, ymax = upper, ymin = lower, group = Type), 
+                stat = 'identity', position = position_dodge(width = 0.1), width = 0.1) + 
+  geom_violin(data = plot_dat %>% filter(x_axis == 4), aes(x=x_axis, y=score, fill = "Other") ,width=0.5) + 
+  geom_errorbar(data=errbar_lims%>% filter(x_axis == 4), 
+                aes(x = x_axis, ymax = upper, ymin = lower, group = Type), 
+                stat = 'identity', position = position_dodge(width = 0.1), width = 0.1) +
+  geom_violin(data = plot_dat %>% filter(x_axis == 4.5), aes(x=x_axis, y=score, fill = "Neutral"),width=0.5) + 
+  geom_errorbar(data=errbar_lims%>% filter(x_axis == 4.5), 
+                aes(x = x_axis, ymax = upper, ymin = lower, group = Type), 
+                stat = 'identity', position = position_dodge(width = 0.1), width = 0.1) + 
+  geom_violin(data = plot_dat %>% filter(x_axis == 6.5), aes(x=x_axis, y=score, fill = "None"),width=0.5) + 
+  geom_errorbar(data=errbar_lims%>% filter(x_axis == 6.5), 
+                aes(x = x_axis, ymax = upper, ymin = lower, group = Type), 
+                stat = 'identity', position = position_dodge(width = 0.1), width = 0.1) + 
+  geom_line(data=errbar_lims %>% filter(Type == "Self"), aes(x = x_axis, y=mean), color="gray") + 
+  geom_line(data=errbar_lims %>% filter(Type == "Other"), aes(x = x_axis, y=mean), color="gray") + 
+  geom_line(data=errbar_lims %>% filter(Type == "Neutral"), aes(x = x_axis, y=mean), color="gray") + 
+  scale_fill_manual(name="Distractor type", 
+                    guide = "legend", 
+                    values = c("Self" = "#E64B35B2", 
+                               "Other" = "#4DBBD5B2", 
+                               "Neutral" = "#00A087B2", 
+                               "None" = "#7E6148B2"), 
+                    breaks = c("Self", "Other", "Neutral", "None")) + 
+  scale_x_continuous(breaks=c(1,4,6.5), labels=c("High-probability", "Low-probability", "No distractor"), limits=c(0, 7)) + 
+  xlab("Distractor position") +
+  ylab("Reaction time(ms)") + 
+  papaja::theme_apa()
+
+ggsave("exp2.png", width = 8, height = 7, dpi = 300)
+
+
+
+
+
+
+
+
+all %>% 
+  group_by(Type) %>% 
+  summarize(mean  = mean(score), 
+            se    = sd(score) / sqrt(n()), 
+            upper = mean + se, 
+            lower = mean - se) %>% 
+  ggplot(aes(x = Type,
+             y = mean, 
+             ymin = lower,
+             ymax = upper)) +
+  geom_col(width = .5, position = position_dodge(.6)) +
+  geom_errorbar(width = .1, position = position_dodge(.6)) +
+  scale_fill_manual(values = c("#737373")) +  xlab("Distractor type") +
+  ylab("Reaction time(ms)") + 
+  theme(
+    plot.margin = unit(c(1, 1, 1, 1), "cm"),
+    panel.background = element_blank(),
+    plot.title = element_text(size = 22, face = "bold",
+                              hjust = 0.5,
+                              margin = margin(b = 15)),
+    axis.line = element_line(color = "black"),
+    axis.title = element_text(size = 22, color = "black",
+                              face = "bold"),
+    axis.text = element_text(size = 22, color = "black"),
+    axis.text.x = element_text(margin = margin(t = 10)),
+    axis.text.y = element_text(size = 17),
+    axis.title.y = element_text(margin = margin(r = 10)),
+    axis.ticks.x = element_blank(),
+    legend.position = c(0.20, 0.8),
+    legend.background = element_rect(color = "black"),
+    legend.text = element_text(size = 15),
+    legend.margin = margin(t = 5, l = 5, r = 5, b = 5),
+    legend.key = element_rect(color = NA, fill = NA)
+  ) + 
+  scale_y_continuous(expand = c(0,0)) +
+  papaja::theme_apa()
+ggsave("exp1.png", width = 8, height = 7, dpi = 300)
