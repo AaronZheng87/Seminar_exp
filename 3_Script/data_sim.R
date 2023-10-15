5fad36b2a46021c37126334782e66e5e890bffba
AaronZheng87 <aaronzheng87@gmail.com>
2023-10-15 19:57:12 +0200

    final figure

--- 3_Script/data_sim.R
+++ 3_Script/data_sim.R
@@ -118,6 +118,26 @@ ggplot(plot_dat) +
   scale_x_continuous(breaks=c(1,4,6.5), labels=c("High-probability", "Low-probability", "No distractor"), limits=c(0, 7)) + 
   xlab("Distractor position") +
   ylab("Reaction time(ms)") + 
+  theme(
+    plot.margin = unit(c(1, 1, 1, 1), "cm"),
+    panel.background = element_blank(),
+    plot.title = element_text(size = 22, face = "bold",
+                              hjust = 0.5,
+                              margin = margin(b = 15)),
+    axis.line = element_line(color = "black"),
+    axis.title = element_text(size = 22, color = "black",
+                              face = "bold"),
+    axis.text = element_text(size = 22, color = "black"),
+    axis.text.x = element_text(margin = margin(t = 10)),
+    axis.text.y = element_text(size = 17),
+    axis.title.y = element_text(margin = margin(r = 10)),
+    axis.ticks.x = element_blank(),
+    legend.position = c(0.20, 0.8),
+    legend.background = element_rect(color = "black"),
+    legend.text = element_text(size = 15),
+    legend.margin = margin(t = 5, l = 5, r = 5, b = 5),
+    legend.key = element_rect(color = NA, fill = NA)
+  ) + 
   papaja::theme_apa()
 
 ggsave("exp2.png", width = 8, height = 7, dpi = 300)
